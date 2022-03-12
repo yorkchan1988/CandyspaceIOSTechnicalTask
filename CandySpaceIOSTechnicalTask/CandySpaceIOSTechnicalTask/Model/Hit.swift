aaa69b1c4d91056b5b69231117e7ba1866e63d17
Chan, York T <york.t.chan@accenture.com>
2022-03-12 17:54:19 +0000

    create data model according to requirements

---
+++ CandySpaceIOSTechnicalTask/CandySpaceIOSTechnicalTask/Model/Hit.swift
@@ -0,0 +1,14 @@
+//
+//  Hit.swift
+//  CandySpaceIOSTechnicalTask
+//
+//  Created by YORK CHAN on 12/3/2022.
+//
+
+import Foundation
+
+struct Hit: Codable {
+    let id: UInt
+    let previewURL: String
+    let largeImageURL: String
+}
---
+++ CandySpaceIOSTechnicalTask/CandySpaceIOSTechnicalTask/Model/SearchResults.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResults.swift
+//  CandySpaceIOSTechnicalTask
+//
+//  Created by YORK CHAN on 12/3/2022.
+//
+
+import Foundation
+
+struct SearchResults: Codable {
+    let hits: [Hit]
+}
