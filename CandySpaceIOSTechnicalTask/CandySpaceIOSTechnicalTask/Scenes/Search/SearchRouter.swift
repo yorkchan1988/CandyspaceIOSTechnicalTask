8c76dce3c62ef2cafe75dbb586879b2939210525
Chan, York T <york.t.chan@accenture.com>
2022-03-13 17:19:48 +0000

    remove unused classes

--- CandySpaceIOSTechnicalTask/CandySpaceIOSTechnicalTask/Scenes/Search/SearchRouter.swift
+++ CandySpaceIOSTechnicalTask/CandySpaceIOSTechnicalTask/Scenes/Search/SearchRouter.swift
@@ -18,9 +18,7 @@ class SearchRouter {
     func goToImagesView(searchResults: SearchResults) {
         let imagesView = ImagesViewController(nibName: "ImagesViewController", bundle: nil)
         let viewModel = ImagesViewModel(datasource: searchResults.hits)
-        let router = ImagesRouter(view: imagesView)
         imagesView.viewModel = viewModel
-        imagesView.router = router
         view?.navigationController?.pushViewController(imagesView, animated: true)
     }
 }
