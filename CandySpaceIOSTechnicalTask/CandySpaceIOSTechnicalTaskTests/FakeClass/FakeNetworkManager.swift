a8d4073866fa71b4caae14e3a80b12b316b1de1c
Chan, York T <york.t.chan@accenture.com>
2022-03-13 21:55:28 +0000

    fix unit test

--- CandySpaceIOSTechnicalTask/CandySpaceIOSTechnicalTaskTests/FakeClass/FakeNetworkManager.swift
+++ CandySpaceIOSTechnicalTask/CandySpaceIOSTechnicalTaskTests/FakeClass/FakeNetworkManager.swift
@@ -12,9 +12,11 @@ class FakeNetworkManager: NetworkManagerProtocol {
     
     let searchResults = SearchResults(hits: [Hit(id: 1127174, previewURL: "https://cdn.pixabay.com/photo/2016/01/08/05/24/sunflower-1127174_150.jpg")])
     
-    func requestData<T: Codable>(apiPath: String, httpMethod: HTTPMethod, parameters: [String : String], responseType: T.Type, completionHandler: @escaping (Result<T, NetworkError>) -> ()) {
-        
-        completionHandler(.success(searchResults as! T))
+    func requestData(apiPath: String, httpMethod: HTTPMethod, parameters:[String:String], completionHandler : @escaping (Result<Data, NetworkError>) -> ()) {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(searchResults) {
+            completionHandler(.success(encoded))
+        }
     }
     
     func requestImage(urlString: String, completionHandler : @escaping (Result<Data, NetworkError>) -> ()) {
